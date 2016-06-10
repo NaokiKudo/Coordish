56712c7589db03926d393f5d85858cd9657ede34
NaokiKudo <rr7usb8qwi2c@gmail.com>
2016-06-10 20:56:29 +0900

    improve GPS

--- app/src/main/java/com/recivilize/naokikudo/coordish/activity/WashRecommendActivity.java
+++ app/src/main/java/com/recivilize/naokikudo/coordish/activity/WashRecommendActivity.java
@@ -1,19 +1,17 @@
 package com.recivilize.naokikudo.coordish.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.recivilize.naokikudo.coordish.GetWeatherForecast;
 import com.recivilize.naokikudo.coordish.R;
@@ -30,6 +28,8 @@ public class WashRecommendActivity extends Activity implements LocationListener 
     private SharedPreferences.Editor editor;
     LocationManager mLocationManager;
     Location location;
+
+    ProgressDialog progressDialog;
 
     GetWeatherForecast getWeatherForecast = new GetWeatherForecast();
 
@@ -54,73 +54,77 @@ public class WashRecommendActivity extends Activity implements LocationListener 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wash__recommend);
+
+        /////////////////////////////////////////////////
+        ///関連づけ
+        /////////////////////////////////////////////////
+
+        //カードView関連
         cardLinear = (LinearLayout) findViewById(R.id.cardLinear);
         inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-
         cardLinear.removeAllViews();
-
+        //ActivityのView
         when = (TextView) findViewById(R.id.when);
         weatherLocation = (TextView) findViewById(R.id.location);
         recommendation = (TextView) findViewById(R.id.recommendation);
 
+        //LocationManagerの取得
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, this);
+        //取得中ダイアログを表示
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setMessage("位置情報を取得しています");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+
+        //location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
         //位置情報を取得
-        requestLocationUpdates();
-        //緯度経度
-        if (location != null) {
-            latitude = location.getLatitude();
-            longitude = location.getLongitude();
-//            位置情報を保存
-            gpsData = getSharedPreferences("SaveGPS", MODE_PRIVATE);
-            editor = gpsData.edit();
-            editor.putFloat("latitude", (float) latitude);
-            editor.putFloat("longitude", (float) longitude);
-            editor.apply();
-            Log.d("GPSSSSS", latitude + "");
-            Log.d("GPSSSSS", longitude + "");
 
-            //天気情報を取得
-            getWeatherForecast.getForecast(gpsData.getFloat("latitude", 0), gpsData.getFloat("longitude", 0), this);
-        } else {
-            Toast.makeText(this, "Cannot get location", Toast.LENGTH_LONG).show();
-        }
+//        //緯度経度
+//        if (location != null) {
+//            latitude = location.getLatitude();
+//            longitude = location.getLongitude();
+////            位置情報を保存
+//            gpsData = getSharedPreferences("SaveGPS", MODE_PRIVATE);
+//            editor = gpsData.edit();
+//            editor.putFloat("latitude", (float) latitude);
+//            editor.putFloat("longitude", (float) longitude);
+//            editor.apply();
+//            Log.d("GPSSSSS", latitude + "");
+//            Log.d("GPSSSSS", longitude + "");
+//
+//            //天気情報を取得
+//            getWeatherForecast.getForecast(gpsData.getFloat("latitude", 0), gpsData.getFloat("longitude", 0), this);
+//        } else {
+//            Toast.makeText(this, "Cannot get location", Toast.LENGTH_LONG).show();
+//        }
 
 
     }
 
 
-    //GPSメソッド群
     @Override
     public void onLocationChanged(Location location) {
 
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        mLocationManager.removeUpdates(this);
+        progressDialog.dismiss();
+
+        getWeatherForecast.getForecast((float)latitude, (float)longitude, this);
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
 
-        switch (status) {
-            case LocationProvider.OUT_OF_SERVICE:
-                break;
-
-            case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                break;
-
-            case LocationProvider.AVAILABLE:
-                if (provider.equals(LocationManager.GPS_PROVIDER)) {
-                    requestLocationUpdates();
-                }
-                break;
-        }
-
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        if (provider.equals(LocationManager.GPS_PROVIDER)) {
-            requestLocationUpdates();
-        }
 
     }
 
@@ -128,24 +132,4 @@ public class WashRecommendActivity extends Activity implements LocationListener 
     public void onProviderDisabled(String provider) {
 
     }
-
-
-    public void requestLocationUpdates() {
-        boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        if (isNetworkEnabled) {
-            mLocationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    LOCATION_UPDATE_MIN_TIME,
-                    LOCATION_UPDATE_MIN_DISTANCE,
-                    this
-            );
-            location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        } else {
-            location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            Toast.makeText(this, "GPS is disabled", Toast.LENGTH_LONG).show();
-            Toast.makeText(this, "Get location from network", Toast.LENGTH_LONG).show();
-        }
-
-    }
-
 }
